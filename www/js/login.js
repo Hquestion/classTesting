f831a074d246cabb5098bdc5623dc343bf9c8b9a
question <1035271660@qq.com>
2019-09-01 21:46:25 +0800

    test

--- www/js/login.js
+++ www/js/login.js
@@ -9,18 +9,29 @@ var vm = new Vue({
     methods: {
         login(auto) {
             var self = this;
-            $.post('http://www.51jyfw.com/BootStrap/Interface/Interface0001.ashx', {
-                auto: '0',
-                ipaddress: '192.168.1.1',
-                psw: this.password,
-                username: this.name
+            $.ajax('http://www.51jyfw.com/BootStrap/Interface/Interface0001.ashx', {
+                data: {
+                    auto: '0',
+                    ipaddress: '192.168.1.1',
+                    psw: this.password,
+                    username: this.name
+                },
+                'contentType': 'application/json'
             }).then(function(res) {
                 console.log(res);
-                localStorage.setItem('name', self.name);
-                localStorage.setItem('password', self.password);
-                localStorage.setItem('user', JSON.stringify(res));
-                // 跳转列表页面
-                location.href = './examList.html';
+                if (res.code === 0) {
+                    localStorage.setItem('name', self.name);
+                    localStorage.setItem('password', self.password);
+                    localStorage.setItem('user', JSON.stringify(res));
+                    document.cookie = 'userId=' + res.FLnkID;
+                    // 跳转列表页面
+                    location.href = './examList.html';
+                } else {
+                    localStorage.removeItem('name');
+                    localStorage.removeItem('password');
+                    localStorage.removeItem('user');
+                    alert(res.msg);
+                }
             }, function () {
                 if (!auto) {
                     alert('登陆失败，请检查账号密码重试！');
